42ca92ccb4334b2f08ee4e37d1f0ef8d7e69a68a
Bru_9940 <bo.ischenko@mail.ru>
2021-08-28 07:54:56 +0300

    minor changes

--- backup.sh
+++ backup.sh
@@ -4,28 +4,22 @@
 # backup script, training
 # $1 - target directory
 # $2 - destination directory
-if [ ! -z $1 ] && [ ! -z $2 ];
+if [ ! -z $1 ] && [ -s $1 ];
 then
-	if [ -s $1 ];
+	printf "target\t\tok\n"
+	if [ ! -z $2 ] && [ -s $2 ];
 	then
-		printf "target\t\tok\n"
-		if [ -s $2 ];
-		then
-			printf "destination\tok\n"
-			timestamp=`date +%H%M%S%d%m%y`
-			aname=${1%/}-$timestamp
-			printf "rolling tarball\t$aname.tar\n"
-			tar -cf $aname.tar $1
-			printf  "gzipping gzip\t$aname.tar.gz\n"
-			gzip -9 $aname.tar
-			printf "moving\t\t$2\n"
-			mv $aname.tar.gz $2
-		else
-			echo "destination... no go"
-			echo "==> check 2nd argument"
-		fi
+		printf "destination\tok\n"
+		aname=${1%/}-`date +%H%M%S%d%m%y`
+		printf "rolling tarball\t$aname.tar\n"
+		tar -cf $aname.tar $1
+		printf  "gzipping gzip\t$aname.tar.gz\n"
+		gzip -9 $aname.tar
+		printf "moving\t\t$2\n"
+		mv $aname.tar.gz $2
 	else
-		echo "target... no go"
-		echo "==> check 1st argument"
+		printf "destination\tno go\n==> check 2nd argument\n"
 	fi
+else
+	printf "target\t\tno go\n==> check 1st argument\n"
 fi
