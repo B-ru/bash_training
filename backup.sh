7f74b29d1854635135b63a44272cefbabbc628e4
Bru_9940 <bo.ischenko@mail.ru>
2021-08-29 08:47:32 +0300

    removed code duplication

--- backup.sh
+++ backup.sh
@@ -6,54 +6,45 @@
 # OPTION	[1..9] compression
 # TARGET	target directory 
 # DESTINATION 	destination directory
-if [ $# = 3 ] && [[ $1 =~ ^[1-9]$  ]];
+if [ $# = 3 ];
 then	
-	if [ -s $2 ];
+	if [[ $1 =~ ^[1-9]$ ]];
 	then
-		printf "target\t\tok\n"
-		if [ -s $3 ];
-		then
-			printf "destination\tok\ncompression\t$1\n"
-			compression=$1
-			target=$2
-			destination=$3
-		else
-			printf "destination\tno go\n==> check 2nd argument\n"
-			exit
-		fi
+		compression=$1
+		target=$2
+		destination=$3
 	else
-		printf "target\t\tno go\n==> check 1st argument\n"
+		printf "compression\tout of range [1..9]\n==>\t\tcheck compression option\n"
 		exit
 	fi
 else
-	if [ $# = 2 ];
-	then
-		if [ -s $1 ];
-		then
-			printf "target\t\tok\n"
-			if [ -s $2 ];
-			then
-				printf "destination\tok\ncompression\tdefault\n"
-				compression=5
-				target=$1
-				destination=$2
-			else
-				printf "destination\tno go\n==> check 2nd argument\n"
-				exit
-			fi
-		else
-			printf "target\t\tno go\n==> check 1st argument\n"
-			exit
-		fi
+	if [ $# = 2 ]
+	then		
+		compression=5
+		target=$1
+		destination=$2
 	else
-		printf "unkown arguments signature: $# arguments\n==> check arguments\n"
+		printf "unkown arguments signature: $# arguments\n==>\t\tcheck arguments\n"
 		exit
 	fi
-fi			
-aname=${target%/}-`date +%H%M%S%d%m%y`
-printf "rolling tarball\t$aname.tar\n"
-tar -cf $aname.tar $target &>/dev/null
-printf  "gzipping\t--//--.gz\n"
-gzip -$compression $aname.tar
-printf "moving to\t$destination\n"
-mv $aname.tar.gz $destination
+fi	
+if [ -s $target ];
+then
+	printf "target\t\tok\n"
+	if [ -s $destination ];
+	then
+		printf "destination\tok\ncompression\t$compression\n"
+		aname=${target%/}-`date +%H%M%S%d%m%y`
+		printf "rolling tarball\t$aname.tar\n"
+		tar -cf $aname.tar $target &>/dev/null
+		printf  "gzipping\t--//--.gz\n"
+		gzip -$compression $aname.tar
+		printf "moving to\t$destination\n"
+		mv $aname.tar.gz $destination
+	else
+		printf "destination\tno go\n==>\t\tcheck 2nd argument\n"
+	fi
+else
+	printf "target\t\tno go\n==>\t\tcheck 1st argument\n"
+	exit
+fi
